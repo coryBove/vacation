896e9bfce6e198d155786302040a0e3ef7822a2d
KENT\cbovein1 <cbovein1@kent.edu>
2017-10-24 13:22:05 -0400

    Split up code

--- launcher/launcher.rb
+++ launcher/launcher.rb
@@ -19,26 +19,3 @@ class Launcher
   end
 
 end
-
-def help
-  print "
-  You must pass in the path to the file to launch.
-
-  Usage: #{__FILE__} target_file
-  "
-end
-
-if ARGV.empty?
-  help
-  exit
-end
-
-app_map = {
-  'html' => 'firefox',
-  'rb' => 'atom',
-  'md' => 'atom'
-}
-
-launcher = Launcher.new app_map
-target = ARGV.join ' '
-launcher.run target
